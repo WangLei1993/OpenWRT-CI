a67f80d94e82a6cea965d8df8765c1f9bc82c8e7
WangLei1993 <cubestone@qq.com>
2025-07-14 12:11:44 +0800

    adjust some patch

--- Patches/Patches.sh
+++ Patches/Patches.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ $WRT_TARGET == "Qualcommax" ]]; then
-	cp ./Patches/Qualcommax/sdhci-msm.patch ./wrt/target/linux/qualcommax/patches-6.6/9999-sdhci-msm.patch
+	cp ./Patches/Qualcommax/sdhci-msm.patch ./wrt/target/linux/qualcommax/patches-6.12/9999-sdhci-msm.patch
 	echo "$WRT_TARGET patch has been installed!"
 fi
